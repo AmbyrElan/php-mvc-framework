a9bad892201db991a0d460c5aba1383c03dbd1be
AmbyrElan <89077791+AmbyrElan@users.noreply.github.com>
2022-06-26 17:19:13 -0400

    render views

--- php-mvc-framework/core/Router.php
+++ php-mvc-framework/core/Router.php
@@ -11,16 +11,19 @@ namespace app\core;
 class Router 
 {
     public Request $request;
+    public Response $response;
     protected array $routes = [];
 
     /** 
      * Router constructor.
      *
      * @param \app\core\Request $request
+     * @param \app\core\Request $response
      */
-    public function __construct(\app\core\Request $request)
+    public function __construct(Request $request, Response $response)
     {
         $this->request = $request;
+        $this->response = $response;
     }
 
     public function get($path, $callback)
@@ -35,11 +38,42 @@ class Router
         $callback = $this->routes[$method][$path] ?? false;
 
         if ($callback === false) {
-            echo "Not found";
-            exit;
+            $this->response->setStatusCode(404);
+            return "Not found";
         }
 
-        echo call_user_func($callback);
+        if (is_string($callback)) {
+            return $this->renderView($callback);
+        }
+
+        return call_user_func($callback);
+    }
+
+    public function renderView($view) 
+    {
+        $layoutContent = $this->layoutContent();
+        $viewContent = $this->renderOnlyView($view);
+        return str_replace('{{content}}', $viewContent, $layoutContent);
+    }
+
+    protected function layoutContent()
+    {
+        // start output cache
+        ob_start();
+
+        // actual output
+        include_once Application::$ROOT_DIR."/views/layouts/main.php";
+
+        // return this instead of outputting to browser
+        // clear buffer
+        return ob_get_clean();
+    }
+
+    protected function renderOnlyView($view)
+    {
+        ob_start();
+        include_once Application::$ROOT_DIR."/views/$view.php";
+        return ob_get_clean();
     }
 
 }
