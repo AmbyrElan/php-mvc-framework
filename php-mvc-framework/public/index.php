257a4da9b33e3873d617fc1ee70f837752def3f9
AmbyrElan <89077791+AmbyrElan@users.noreply.github.com>
2022-07-11 09:09:09 -0400

    render login & register pages

--- php-mvc-framework/public/index.php
+++ php-mvc-framework/public/index.php
@@ -4,6 +4,8 @@
  * @author AmbyrElan <89077791+AmbyrElan@users.noreply.github.com>
  */
 require_once __DIR__ .'/../vendor/autoload.php'; 
+
+use \app\controllers\AuthController;
 use \app\controllers\SiteController;
 use app\core\Application;
 
@@ -14,5 +16,10 @@ $app->router->get('/', [SiteController::class, 'home']);
 $app->router->get('/contact', [SiteController::class, 'contact']);
 
 $app->router->post('/contact', [SiteController::class, 'handleContact']);
+
+$app->router->get('/login', [AuthController::class, 'login']);
+$app->router->post('/login', [AuthController::class, 'login']);
+$app->router->get('/register', [AuthController::class, 'register']);
+$app->router->post('/register', [AuthController::class, 'register']);
 
 $app->run();
