516470dd092ecc6e602fc0bc6625f398ffedccba
Ambyr <ambyrelan@wayne.edu>
2024-08-25 19:44:13 +0000

    initial setup

---
+++ index.php
@@ -0,0 +1,29 @@
+<?php
+
+/** 
+ * AmbyrElan
+ * 5/14/2022
+ *
+ *  Leftoff at 21:08
+ */
+
+require_once __DIR__.'/vendor/autoload.php';
+
+use app\core\Application;
+
+// Create new application
+$app = new Application();
+
+// Return when / is run
+$app->router->get('/', function(){
+    return 'Hello World';
+}); 
+
+$app->router->get('/contact', function(){
+    return 'Contact';
+}); 
+
+// Handle everything
+$app->run();
+
+ 
