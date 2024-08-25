189cbf176e5ca450dc8e0d98896cd16db4a61c78
Ambyr <ambyrelan@wayne.edu>
2024-08-25 20:18:44 +0000

    composer & autoloader

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => '526b42ef04d5f4cb3f6ed914feaba6a3bd7a1421',
+        'name' => 'ambyrelan/php-mvc-framework',
+        'dev' => true,
+    ),
+    'versions' => array(
+        'ambyrelan/php-mvc-framework' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => '526b42ef04d5f4cb3f6ed914feaba6a3bd7a1421',
+            'dev_requirement' => false,
+        ),
+    ),
+);
